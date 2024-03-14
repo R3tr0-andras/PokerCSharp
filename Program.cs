f0377822f2302d3d8e1da034ba52cf8008aa77c9
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-03-14 14:42:45 +0100

    interract

--- Program.cs
+++ Program.cs
@@ -1,31 +1,30 @@
-﻿namespace PokerCSharp
+﻿using System;
+
+namespace PokerCSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int joueurActuel = 4;
-
             //Mes librairies
             Card traitementCard = new Card();
             ia player = new ia();
+            interact ui = new interact();
 
+            //Mise en place des cartes
             List<string> suits = new List<string> { "Coeur", "Carreau", "Trèfle", "Pique" };
             List<string> ranks = new List<string> { "As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Roi", "Reine", "Valet" };
-
             List<string> deck = traitementCard.CreateDeck(suits, ranks);
             traitementCard.ShuffleDeck(deck);
 
+            //Mise en place des joueurs
+            int joueurActuel = ui.NumberPlayer();
             List<string> players = new List<string>();
 
-
-            //foreach (string decks in deck)
-            //{
-            //Console.WriteLine(decks);
-            //}
-
+            //Distribution de deux cartes par joueurs
             string[,] handPlayer = player.initializeAndDistribute(joueurActuel, deck);
-            // Afficher la matrice
+
+            // Afficher les mains
             for (int i = 0; i < joueurActuel; i++)
             {
                 for (int j = 0; j < 2; j++)
@@ -35,6 +34,10 @@
                 Console.WriteLine();
             }
 
+            //Mise des cartes sur la table
+
+            //3 cartes puis une par une jusqu'à 5 cartes
+            
 
         }
     }
---
+++ interact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PokerCSharp
+{
+    public struct interact
+    {
+        public int NumberPlayer()
+        {
+            int nbj = 0;
+            do
+            {
+                Console.WriteLine("Combien de joueur voulez-vous avoir autour de la table ? (4 minimum et 6 maximum)");
+                string nb = Console.ReadLine();
+
+                if (!int.TryParse(nb, out nbj))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Veuillez entrer un nombre valide.");
+                    continue; 
+                }
+
+                if (nbj < 4 || nbj > 6)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Veuillez entrer un nombre entre 4 et 6.");
+                    continue; 
+                }
+
+                Console.WriteLine(nbj);
+                break; // Exit
+            } while (true); 
+
+            return nbj;
+        }
+    }
+}
